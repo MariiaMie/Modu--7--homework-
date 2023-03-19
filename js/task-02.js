92a6bc72c379b1d3cf9be7220617a5676897e7ff
MariiaMie <mashamelnikova94@gmail.com>
2023-03-19 16:07:18 +0100

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const ulList = document.querySelector("#ingredients");
+const liList = ingredients.forEach((ingredient) => {
+	let items = document.createElement("li");
+	items.innerHTML = ingredient;
+	ulList.appendChild(items);
+});
+
+console.log(ulList);
