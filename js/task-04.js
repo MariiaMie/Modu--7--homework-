102ca73c33dd6c2d2385958bb5ab0ceaa13129b6
MariiaMie <mashamelnikova94@gmail.com>
2023-03-23 19:12:56 +0100

    Task 4 Homework

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,35 @@
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBoxBtn = controls.querySelector("[data-create]");
+const destroyBoxBtn = controls.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createBoxBtn.addEventListener("click", () => {
+	const amount = parseInt(document.querySelector("input").value);
+	createBoxes(amount);
+});
+destroyBoxBtn.addEventListener("click", () => {
+	destroyBoxes();
+});
+
+function createBoxes(amount) {
+	let boxSize = 30;
+	for (let i = 0; i < amount; i++) {
+		const box = document.createElement("div");
+		box.style.width = `${boxSize}px`;
+		box.style.height = `${boxSize}px`;
+		box.style.backgroundColor = getRandomHexColor();
+		boxSize += 10;
+		boxesContainer.appendChild(box);
+	}
+}
+
+function destroyBoxes() {
+	boxesContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
